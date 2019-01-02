734ea24c027c7a8a749b1e3c5671fa247a1844fc
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 15:56:26 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length 
+      if yield(collection[i]) == true 
+        new_collection.push(collection[i])
+      end 
+      i += 1 
+    end
+    new_collection
+  else 
+    puts "Please use a block."
+  end 
 end
